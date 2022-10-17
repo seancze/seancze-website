9caf5c2919d53111256f5e2774049f8a1e2a097e
Sean <seancze@gmail.com>
2022-10-17 00:34:25 -0400

    feat: add skills section

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
+import { DbService } from '../db.service';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.scss']
+  styleUrls: ['./skills.component.scss'],
 })
 export class SkillsComponent implements OnInit {
+  techStackImages: { [key: string]: string } = {};
+  hover: number = -1;
 
-  constructor() { }
+  constructor(private dbService: DbService) {}
 
   ngOnInit(): void {
-  }
+    // deep copy techStackImages before mutation below
+    this.techStackImages = JSON.parse(
+      JSON.stringify(this.dbService.getTechStackImages())
+    );
 
+    for (const key in this.techStackImages) {
+      // manually delete skills with no images or poorly formatted images
+      if (
+        key === 'Wix' ||
+        key === 'humanPowered' ||
+        key === 'Lucid' ||
+        key === 'IBM Watson'
+      ) {
+        delete this.techStackImages[key];
+      }
+    }
+  }
 }
