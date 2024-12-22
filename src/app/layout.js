57e54f1d572cd66e1d2cd9c1fbf40ec569e1dfca
seancze <60085582+seancze@users.noreply.github.com>
2024-12-23 01:01:33 +0800

    refactor: remove redundant code

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,17 +4,6 @@ import { HotjarInitialiser } from "@/app/components/HotjarInitialiser";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 export const metadata = {
   title: "Sean Chen",
   description: "Sean Chen portfolio website",
@@ -23,9 +12,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         {children}
         <HotjarInitialiser />
         <Analytics />
