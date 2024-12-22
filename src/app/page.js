c09687e90dd62e88a4b769530ddd29893cc9a2ac
seancze <60085582+seancze@users.noreply.github.com>
2024-12-23 01:57:33 +0800

    fix: typewriter still temporarily appearing with wrong font initially

--- src/app/page.js
+++ src/app/page.js
@@ -11,8 +11,10 @@ import Typewriter from "typewriter-effect";
 export default function Home() {
   const [currentSection, setCurrentSection] = useState("home");
   const [isMagicMode, setIsMagicMode] = useState(false);
+  // since the overlay is visible by default, the showTypewriter state ensures that the overlay is only shown when magic mode button is clicked
+  const [showTypewriter, setShowTypewriter] = useState(false);
   const [hasShownTypewriter, setHasShownTypewriter] = useState(false);
-  const [isTypewriterDone, setIsTypewriterDone] = useState(false);
+  // the overlay state ensures that the overlay persists for a brief moment after the typewriter effect is done
   const [isOverlayVisible, setIsOverlayVisible] = useState(true);
 
   const scrollToSection = (section) => {
@@ -21,20 +23,12 @@ export default function Home() {
   };
 
   const toggleMagicMode = () => {
-    if (!isMagicMode && !hasShownTypewriter) {
-      setHasShownTypewriter(true);
-    }
     setIsMagicMode(!isMagicMode);
-  };
-
-  useEffect(() => {
-    if (isTypewriterDone) {
-      // Wait a brief moment after typing is done before starting fade out
-      setTimeout(() => {
-        setIsOverlayVisible(false);
-      }, 500);
+    // this ensures that the overlay is only shown the first time magic mode is toggled
+    if (!isMagicMode && !hasShownTypewriter) {
+      setShowTypewriter(true);
     }
-  }, [isTypewriterDone]);
+  };
 
   return (
     <main
@@ -48,19 +42,26 @@ export default function Home() {
           
           `}
     >
-      {hasShownTypewriter && (
+      {/* NOTE: doing '&& isOverlayVisible' will not show the overlay fading out*/}
+      {/* this is because the component is removed completely when isOverlayVisible is set to false */}
+      {/* in contrast, setting opacity to 0 allows us to see the transition from opacity-100 to opacity-0 */}
+      {showTypewriter && (
         <div
           className={`fixed inset-0 bg-black z-50 flex items-center justify-center transition-opacity duration-1000 ease-in-out ${
             !isOverlayVisible ? "opacity-0 pointer-events-none" : "opacity-100"
           }`}
         >
-          <div className="text-artyclick-amber font-halloween sm:text-[1.75rem] lg:text-[3rem] sm:whitespace-nowrap">
+          <div className="text-artyclick-amber sm:text-[1.75rem] lg:text-[3rem] sm:whitespace-nowrap">
             <Typewriter
               onInit={(typewriter) => {
                 typewriter
                   .typeString("I solemnly swear that I am up to no good...")
                   .callFunction(() => {
-                    setIsTypewriterDone(true);
+                    setHasShownTypewriter(true);
+                    // Wait a brief moment after typing is done before starting fade out
+                    setTimeout(() => {
+                      setIsOverlayVisible(false);
+                    }, 500);
                   })
                   .start();
               }}
