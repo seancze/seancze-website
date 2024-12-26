fdb59b31b2586bc2fc01b67dacda8ec212682623
seancze <60085582+seancze@users.noreply.github.com>
2024-12-26 16:12:44 +0800

    feat: add image skeleton component to mischiefs component

--- src/app/components/Mischiefs.js
+++ src/app/components/Mischiefs.js
@@ -27,6 +27,7 @@ const MischiefsSlider = () => {
   const [currentImageIndex, setCurrentImageIndex] = useState(1);
   const [isAnimating, setIsAnimating] = useState(false);
   const [transitionEnabled, setTransitionEnabled] = useState(true);
+  const [imagesLoaded, setImagesLoaded] = useState({});
 
   const nextImage = () => {
     if (isAnimating) return;
@@ -74,9 +75,35 @@ const MischiefsSlider = () => {
     setIsAnimating(false);
   };
 
+  const handleImageLoad = (index) => {
+    setImagesLoaded((prev) => ({
+      ...prev,
+      [index]: true,
+    }));
+  };
+
+  // credits: https://flowbite.com/docs/components/skeleton/
+  const ImageSkeleton = () => (
+    <div
+      role="status"
+      className="flex items-center justify-center h-full w-full bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700"
+    >
+      <svg
+        className="w-10 h-10 text-gray-200 dark:text-gray-600"
+        aria-hidden="true"
+        xmlns="http://www.w3.org/2000/svg"
+        fill="currentColor"
+        viewBox="0 0 20 18"
+      >
+        <path d="M18 0H2a2 2 0 0 0-2 2v14a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2Zm-5.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm4.376 10.481A1 1 0 0 1 16 15H4a1 1 0 0 1-.895-1.447l3.5-7A1 1 0 0 1 7.468 6a.965.965 0 0 1 .9.5l2.775 4.757 1.546-1.887a1 1 0 0 1 1.618.1l2.541 4a1 1 0 0 1 .028 1.011Z" />
+      </svg>
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
+
   return (
     <div className="w-full">
-      <div className={`relative overflow-hidden rounded-xl h-64`}>
+      <div className="relative overflow-hidden rounded-xl h-64">
         <div
           className={`flex gap-4 ${
             transitionEnabled
@@ -93,15 +120,19 @@ const MischiefsSlider = () => {
           {slides.map((slide, index) => (
             <div
               key={`${slide.image}-${index}`}
-              className={`flex-shrink-0 rounded-xl overflow-hidden w-96`}
+              className="flex-shrink-0 rounded-xl overflow-hidden w-96"
             >
-              <div className={`relative h-64`}>
+              <div className="relative h-64">
+                {!imagesLoaded[index] && <ImageSkeleton />}
                 <Image
                   src={slide.image}
                   alt={slide.alt}
                   fill
-                  className="object-cover"
+                  className={`object-cover ${
+                    imagesLoaded[index] ? "opacity-100" : "opacity-0"
+                  }`}
                   priority={index <= 2}
+                  onLoad={() => handleImageLoad(index)}
                 />
               </div>
             </div>
